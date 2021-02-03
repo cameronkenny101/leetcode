dc83455ec649ee61164c53323f5ee5fbc9c6d2ee
Cameron Kenny <cam.e.kenny@gmail.com>
2021-02-03 20:15:37 +0000

    Longest Substring

--- AddTwoNumbers/src/AddTwoNumbers.java
+++ AddTwoNumbers/src/AddTwoNumbers.java
@@ -13,41 +13,23 @@ public class AddTwoNumbers {
         boolean carry = false;
         int digit;
         while (l1.next != null && l2.next != null && !carry) {
-            ListNode answer = new ListNode();
-            ListNode head = answer;
-            boolean carry = false;
-            boolean listOne = true;
-            boolean listTwo = true;
-            int digit = 0;
-            while (listOne || listTwo || carry) {
-                if(listOne)
-                    digit += l1.val;
-                if(listTwo)
-                    digit += l2.val;
-                if(carry)
-                    digit +=1;
+            if(carry)
+                digit = l1.val + l2.val + 1;
+            else
+                digit = l1.val + l2.val;
 
-                if(digit >= 10) {
-                    carry = true;
-                    digit %= 10;
-                } else
-                    carry = false;
+            if(digit > 10) {
+                carry = true;
+                digit %= 10;
+            } else
+                carry = false;
 
-                answer.val = digit;
-                if(l1.next != null)
-                    l1 = l1.next;
-                else
-                    listOne = false;
-                if(l2.next != null)
-                    l2 = l2.next;
-                else
-                    listTwo = false;
-                if(listOne || listTwo || carry) {
-                    answer.next = new ListNode();
-                    answer = answer.next;
-                }
-                digit = 0;
-            }
-            return head;
+            answer.val = digit;
+            answer.next = new ListNode();
+            answer = answer.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        return answer;
     }
 }
---
+++ LongestSubstring/src/LongestSubstring.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class LongestSubstring {
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
+    }
+
+    static int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> map= new HashMap<>();
+        int maxLength = 0;
+        int length = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            if(map.containsKey(s.charAt(i))) {
+                i = map.remove(s.charAt(i));
+                length = 0;
+                map.clear();
+            } else {
+                map.put(s.charAt(i), i);
+                length++;
+                if(length > maxLength)
+                    maxLength = length;
+            }
+        }
+        return maxLength;
+    }
+}
