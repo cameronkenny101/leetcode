2b93b33a076a85dfa6494f4712b5d5b80aee9295
cameronkenny101 <cam.e.kenny@gmail.com>
2021-03-04 15:56:22 +0000

    AlmostPalindrome solved

---
+++ AlmostPalindrome/src/AlmostPalindrome.java
@@ -0,0 +1,25 @@
+public class AlmostPalindrome {
+
+    public static void main(String[] args) {
+        System.out.println(almostPalindrome("abcxba"));
+    }
+
+    public static String almostPalindrome(String input) {
+        int i = 0;
+        int j = input.length() - 1;
+        StringBuilder ans = new StringBuilder(input);
+        while(i != j) {
+            if(i + 1 == j && ans.charAt(i) != ans.charAt(j)) {
+                ans.insert(i, ans.charAt(j));
+                break;
+            }
+            if(ans.charAt(i) != ans.charAt(j)) {
+                ans.insert(j + 1, ans.charAt(i));
+                break;
+            }
+            i++;
+            j--;
+        }
+        return String.valueOf(ans);
+    }
+}
