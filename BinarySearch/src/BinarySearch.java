0ac820b44ea99a8665839ab127801f7a404e6dcf
cameronkenny101 <cam.e.kenny@gmail.com>
2021-04-01 13:04:58 +0100

    Binary Search solved

---
+++ BinarySearch/src/BinarySearch.java
@@ -0,0 +1,22 @@
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 2, 5, 6, 9, 12};
+        System.out.println(search(nums, 2));
+    }
+
+    public static int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] > target)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return -1;
+    }
+}
