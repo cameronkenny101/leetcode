856ec15143595a5499c29d640413ee9b02edd726
cameronkenny101 <cam.e.kenny@gmail.com>
2021-04-21 16:52:32 +0100

    Leetcode solution to minCostToClimbStairs

---
+++ minCostClimbStairs/src/minCost.java
@@ -0,0 +1,14 @@
+public class minCost {
+
+    public static void main(String[] args) {
+        int[] arr = {1,100,1,1,1,100,1,1,100,1};
+        System.out.println(minCostClimbingStairs(arr));
+    }
+
+    private static int minCostClimbingStairs(int[] cost) {
+        for(int i = 2; i < cost.length; i++) {
+            cost[i] += Math.min(cost[i - 1], cost[i - 2]);
+        }
+        return Math.min(cost[cost.length - 1], cost[cost.length - 2]);
+    }
+}
