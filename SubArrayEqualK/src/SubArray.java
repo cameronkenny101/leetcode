d317460844d879dca07c69b87f2e4da4d66379db
cameronkenny101 <cam.e.kenny@gmail.com>
2021-05-12 14:39:01 +0100

    Solved subArraySumK leetcode medium problem

--- SubArrayEqualK/src/SubArray.java
+++ SubArrayEqualK/src/SubArray.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class SubArray {
 
     public static void main(String[] args) {
@@ -8,15 +10,17 @@ public class SubArray {
     private static int subarraySum(int[] nums, int k) {
         int count = 0;
         int ans = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
         for(int i = 0; i < nums.length; i++) {
-            count = nums[i];
-            if(count == k)
-                ans++;
-            for(int j = i + 1; j < nums.length; j++) {
-                count += nums[j];
-                if(count == k)
-                    ans++;
-            }
+            count += nums[i];
+
+            if(map.containsKey(count - k))
+                ans += map.get(count - k);
+
+            map.put(count, map.getOrDefault(count, 0) + 1);
+
         }
         return ans;
     }
