649f9015393353df5083d426dc46d91bf77b8ba4
cameronkenny101 <cam.e.kenny@gmail.com>
2021-05-25 17:14:26 +0100

    Completed HouseRobber leetcode meduim question

---
+++ HouseRobber/src/HouseRobber.java
@@ -0,0 +1,19 @@
+public class HouseRobber {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        System.out.println(rob(nums));
+    }
+
+    private static int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for(int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
+        }
+
+        return dp[dp.length - 1];
+    }
+}
