3f7f3a1cc0ffd415f16180bb26c88a8204492036
cameronkenny101 <cam.e.kenny@gmail.com>
2021-05-31 16:23:31 +0100

    Completed PathSum leetcode question

---
+++ HasPathSum/src/HasPathSum.java
@@ -0,0 +1,25 @@
+public class HasPathSum {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null)
+            return false;
+        if (root.left == null && root.right == null && root.val == sum)
+            return true;
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+
+
+}
