8db5886f5963f9ae8f577e5be906ba72f0360ec8
cameronkenny101 <cam.e.kenny@gmail.com>
2021-06-14 16:03:21 +0100

    Completed coin change leetcode medium question

--- CoinChange/src/Change.java
+++ CoinChange/src/Change.java
@@ -1,23 +1,24 @@
+import java.util.Arrays;
+
 public class Change {
 
     public static void main(String[] args) {
-        int[] coins = {186,419,83,408};
-        System.out.println(coinChange(coins, 6249));
+        int[] coins = {1};
+        System.out.println(coinChange(coins, 2));
     }
 
     public static int coinChange(int[] coins, int amount) {
-        int count = 0;
-        while (amount > 0) {
-            int maxCoin = 0;
-            for(int i = 0; i < coins.length; i++) {
-                if (coins[i] <= amount && coins[i] > maxCoin)
-                    maxCoin = coins[i];
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for(int i = 1; i <= amount; i++) {
+            for(int coin : coins) {
+                if(coin <= i) {
+                    dp[i] = Math.min(1 + dp[i - coin], dp[i]);
+                }
             }
-            if(maxCoin == 0)
-                return -1;
-            count += amount / maxCoin;
-            amount %= maxCoin;
         }
-        return count;
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
+
     }
 }
